37be9ea5e83cedbb817cb0efa5d06aeda953d0ed
Flavio <flavio.karr@roxcode.dev>
2025-06-22 11:23:05 -0300

    criei os voids de movimento

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -44,9 +46,15 @@ class HomePage extends StatelessWidget {
             ),
             const SizedBox(height: 80),
 
-            criarBotaoNivel(context, 4, Colors.indigo.shade300, 'Fácil'),
-            criarBotaoNivel(context, 5, Colors.purple.shade300, 'Médio'),
-            criarBotaoNivel(context, 6, Colors.deepPurple.shade400, 'Difícil'),
+            criarBotaoNivel(context, 4, Colors.indigo.shade300, 'Fácil', 1024),
+            criarBotaoNivel(context, 5, Colors.purple.shade300, 'Médio', 2048),
+            criarBotaoNivel(
+              context,
+              6,
+              Colors.deepPurple.shade400,
+              'Difícil',
+              4096,
+            ),
           ],
         ),
       ),
@@ -58,6 +66,7 @@ class HomePage extends StatelessWidget {
     int gridSize,
     Color cor,
     String nivelTexto,
+    int objetivo,
   ) {
     return Padding(
       padding: EdgeInsets.all(8),
@@ -69,7 +78,7 @@ class HomePage extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
           ),
         ),
-        onPressed: () => _iniciarJogo(context, gridSize, nivelTexto),
+        onPressed: () => _iniciarJogo(context, gridSize, nivelTexto, objetivo),
         child: Text(
           nivelTexto,
           style: TextStyle(
@@ -82,12 +91,21 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  void _iniciarJogo(BuildContext context, int gridSize, String nivelTexto) {
+  void _iniciarJogo(
+    BuildContext context,
+    int gridSize,
+    String nivelTexto,
+    int objetivo,
+  ) {
     Navigator.push(
       context,
       MaterialPageRoute(
         builder:
-            (context) => PaginaJogo(gridSize: gridSize, nivelTexto: nivelTexto),
+            (context) => PaginaJogo(
+              gridSize: gridSize,
+              nivelTexto: nivelTexto,
+              objetivo: objetivo,
+            ),
       ),
     );
   }
@@ -96,11 +114,13 @@ class HomePage extends StatelessWidget {
 class PaginaJogo extends StatefulWidget {
   final int gridSize;
   final String nivelTexto;
+  final int objetivo;
 
   const PaginaJogo({
     super.key,
     required this.gridSize,
     required this.nivelTexto,
+    required this.objetivo,
   });
 
   @override
@@ -108,6 +128,189 @@ class PaginaJogo extends StatefulWidget {
 }
 
 class _PaginaJogoState extends State<PaginaJogo> {
+  late List<List<int>> grid;
+  int moves = 0;
+  bool perdeu = false;
+  bool ganhou = false;
+  int max = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    startGame();
+  }
+
+  void startGame() {
+    grid = List.generate(
+      widget.gridSize,
+      (_) => List.filled(widget.gridSize, 0),
+    );
+    addNovoNumero();
+    addNovoNumero();
+  }
+
+  void addNovoNumero() {
+    List<Point<int>> espacosVazios = [];
+
+    for (int i = 0; i < widget.gridSize; i++) {
+      for (int j = 0; j < widget.gridSize; j++) {
+        if (grid[i][j] == 0) {
+          espacosVazios.add(Point(i, j));
+        }
+      }
+    }
+
+    if (espacosVazios.isNotEmpty) {
+      Random random = Random();
+      Point<int> spot = espacosVazios[random.nextInt(espacosVazios.length)];
+      grid[spot.x][spot.y] = 2;
+    }
+  }
+
+  void moveCima() {
+    if (ganhou || perdeu) return;
+
+    bool moved = false;
+
+    for (int j = 0; j < widget.gridSize; j++) {
+      for (int i = 1; i < widget.gridSize; i++) {
+        if (grid[i][j] != 0) {
+          int k = i;
+          while (k > 0 && grid[k - 1][j] == 0) {
+            grid[k - 1][j] = grid[k][j];
+            grid[k][j] = 0;
+            k--;
+            moved = true;
+          }
+
+          if (k > 0 && grid[k - 1][j] == grid[k][j]) {
+            grid[k - 1][j] *= 2;
+            grid[k][j] = 0;
+            moved = true;
+            checkVitoria();
+          }
+        }
+      }
+    }
+
+    if (moved) {
+      setState(() {
+        moves++;
+        addNovoNumero();
+        checkDerrota();
+      });
+    }
+  }
+
+  void moveBaixo() {
+    if (ganhou || perdeu) return;
+
+    bool moved = false;
+
+    for (int j = 0; j < widget.gridSize; j++) {
+      for (int i = widget.gridSize - 2; i >= 1; i--) {
+        if (grid[i][j] != 0) {
+          int k = i;
+          while (k < widget.gridSize - 1 && grid[k + 1][j] == 0) {
+            grid[k + 1][j] = grid[k][j];
+            grid[k][j] = 0;
+            k++;
+            moved = true;
+          }
+
+          if (k < widget.gridSize && grid[k + 1][j] == grid[k][j]) {
+            grid[k + 1][j] *= 2;
+            grid[k][j] = 0;
+            moved = true;
+            checkVitoria();
+          }
+        }
+      }
+    }
+
+    if (moved) {
+      setState(() {
+        moves++;
+        addNovoNumero();
+        checkDerrota();
+      });
+    }
+  }
+
+  void moveEsquerda() {
+    if (ganhou || perdeu) return;
+
+    bool moved = false;
+
+    for (int i = 0; i < widget.gridSize; i++) {
+      for (int j = 1; j < widget.gridSize; j++) {
+        if (grid[i][j] != 0) {
+          int k = i;
+          while (k > 0 && grid[i][k - 1] == 0) {
+            grid[i][k - 1] = grid[i][k];
+            grid[i][k] = 0;
+            k--;
+            moved = true;
+          }
+
+          if (k > 0 && grid[i][k - 1] == grid[i][k]) {
+            grid[i][k - 1] *= 2;
+            grid[i][k] = 0;
+            moved = true;
+            checkVitoria();
+          }
+        }
+      }
+    }
+
+    if (moved) {
+      setState(() {
+        moves++;
+        addNovoNumero();
+        checkDerrota();
+      });
+    }
+  }
+
+  void moveDireira() {
+    if (ganhou || perdeu) return;
+
+    bool moved = false;
+
+    for (int i = 0; i < widget.gridSize; i++) {
+      for (int j = 1; j < widget.gridSize; j++) {
+        if (grid[i][j] != 0) {
+          int k = j;
+          while (k > 0 && grid[i][k - 1] == 0) {
+            grid[i][k - 1] = grid[i][k];
+            grid[i][k] = 0;
+            k--;
+            moved = true;
+          }
+
+          if (k > 0 && grid[i][k - 1] == grid[i][k]) {
+            grid[i][k - 1] *= 2;
+            grid[i][k] = 0;
+            moved = true;
+            checkVitoria();
+          }
+        }
+      }
+    }
+
+    if (moved) {
+      setState(() {
+        moves++;
+        addNovoNumero();
+        checkDerrota();
+      });
+    }
+  }
+
+  void checkVitoria() {}
+
+  void checkDerrota() {}
+
   Widget criarBotaoMove(BuildContext context, IconData icone) {
     return Container(
       height: 70,
